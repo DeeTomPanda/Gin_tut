22ba4a9608dbbffade60fcc3973ee47d79ba90ed
Dtompanda <thewalkerdarkknight@gmail.com>
2023-12-18 19:05:50 +0530

    POST Request with JSON body and struct tags

--- go_web/example.go
+++ go_web/example.go
@@ -5,19 +5,35 @@ import (
     "fmt"
 )
 
- func getRoute(c *gin.Context){
-    fmt.Println(c.Params)
+type Problem struct{
+    Name string `json:"name"`
+    Tags []string `json:"tags"`
+}
+
+func getData(c *gin.Context){
+    
+    var problem Problem
+    if err:=c.ShouldBindJSON(&problem); err!=nil{
+        fmt.Println("Error while parsing")
+        c.JSON(404,gin.H{
+            "Error":err.Error(),
+        })
+        return
+    }
+    fmt.Println(problem.Tags[0])
     c.JSON(200,gin.H{
-            "Woo":"Hoo",
-            "Hooo":"Wooo",
+        "status":"ok",
     })
+
 }
 
 func main() {
     // Initialize the Gin router
     r := gin.Default()
-    r.GET("/",getRoute)
-    r.GET("/:name",getRoute)
+    r.GET("/",func (c *gin.Context){
+        c.String(200,"Ok!")
+    })
+    r.POST("/problem",getData)
     // Run the server on port 8080
     r.Run(":8080")
 }
